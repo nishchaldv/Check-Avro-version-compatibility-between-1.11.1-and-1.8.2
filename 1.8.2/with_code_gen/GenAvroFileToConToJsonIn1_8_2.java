0ac90ca62e3d5b0f175291d8fb56ddcc39f97aac
nishchaldv <83815115+nishchaldv@users.noreply.github.com>
2023-04-26 15:17:59 +0530

    Update GenAvroFileToConToJsonIn1_8_2.java

--- 1.8.2/with_code_gen/GenAvroFileToConToJsonIn1_8_2.java
+++ 1.8.2/with_code_gen/GenAvroFileToConToJsonIn1_8_2.java
@@ -41,7 +41,7 @@ public class GenAvroFileToConToJsonIn1_8_2 {
       DatumWriter<emp> empDatumWriter = new SpecificDatumWriter<emp>(emp.class);
       DataFileWriter<emp> empFileWriter = new DataFileWriter<emp>(empDatumWriter);
 	
-      empFileWriter.create(e1.getSchema(), new File("/Users/ndv/hadoop/avrofiles/AvroFileGenIn1_8_2ToConvertToJsonin1_11_1.avro"));
+      empFileWriter.create(e1.getSchema(), new File("/path/to/avrofiles/AvroFileGenIn1_8_2ToConvertToJsonin1_11_1.avro"));
 	
       empFileWriter.append(e1);
       empFileWriter.append(e2);
